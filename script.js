13cdf48d66d34c055c8982431073ab85d00f3d8f
Arina231 <arinahonaa119@gmail.com>
2023-07-11 21:05:33 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'correct number';
-document.querySelector('.guess').value = 13;
