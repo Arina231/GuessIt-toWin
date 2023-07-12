2c132b35aa90bfaab5ccb16789326340192c9a07
Arina231 <arinahonaa119@gmail.com>
2023-07-13 00:10:26 +0100

    domcss

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  /// no input///
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!!';
+    //// player wins///
   } else if (guess === secretNumber) {
     console.log(
       (document.querySelector('.message').textContent = 'Correct Number!')
     );
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    /// low guess///
   } else if (guess < secretNumber) {
     if (score > 1) {
       console.log(
@@ -24,6 +28,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Game Over';
       document.querySelector('.score').textContent = 0;
     }
+    ///// high guess////
   } else if (guess > secretNumber) {
     if (score > 1) {
       console.log(
